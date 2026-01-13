0e8cd5bbbc5e63bb95820d69e1cd8b9057c8845b
easylearningbd <elebdsoft@gmail.com>
2026-01-14 04:33:30 +0600

    47 User Logout Part 2

--- app/api/auth/signout/route.ts
+++ app/api/auth/signout/route.ts
@@ -1,6 +1,7 @@
 import { signOut } from "@/lib/auth";
 import { NextRequest, NextResponse } from "next/server";
 import { cookies } from "next/headers";
+  
 
 export async function POST(request: NextRequest) {
     try {
@@ -14,8 +15,21 @@ export async function POST(request: NextRequest) {
             }
         });
 
+    // Sign out without redirect 
+    await signOut({ redirect: false });
 
+    return NextResponse.json({ success:true },{
+        headers:{
+            'Cache-Control' : 'no-store, no-cache, must-revalidate, proxy-revalidate',
+            'Pragma': 'no-cache',
+            'Expires': '0',
+        }
+    });
     } catch (error) {
-        
+        console.error("Sign out error",error);
+        return NextResponse.json(
+            {error: "Filed to sign out"},
+            { status: 500}
+        );
     }
 }
